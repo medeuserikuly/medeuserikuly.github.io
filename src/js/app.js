c2f565a409b2d7e425f39b988956988703083d3a
Medeu <63974696+medeuserikuly@users.noreply.github.com>
2023-06-30 11:40:00 +0600

    Add files via upload

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+import navMenu from "./modules/navMenu.js";
+import isWebp from "./modules/webp.js";
+
+window.addEventListener("DOMContentLoaded", () => {
+    'use strict';
+
+    isWebp();
+    navMenu();
+    
+});
